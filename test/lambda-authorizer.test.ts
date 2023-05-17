ad77bc71c2dd8015ad0b265326703197de718c9d
Asahi Miura <miuraasahi1@gmail.com>
2023-05-17 21:54:59 +0900

    Lambda Authorizerの単体テストを作成

---
+++ test/lambda-authorizer.test.ts
@@ -0,0 +1,39 @@
+import { handler } from "../src/lambda/lambda-authorizer";
+import { APIGatewayAuthorizerResult } from 'aws-lambda';
+import * as dotenv from 'dotenv';
+
+// Todo: 他のテスト時にスキップする
+
+dotenv.config();
+
+const event = {
+  type: "TOKEN",
+  authorizationToken: process.env.TEST_TOKEN
+}
+
+describe('Lambda_Authorizer_Function', (): void => {
+  it('response policy', async () => {
+
+    const expected = {
+      policyDocument: {
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: undefined,
+          },
+        ],
+        Version: '2012-10-17',
+      },
+    };
+
+    const result: APIGatewayAuthorizerResult = await handler(event as any);
+
+    // principalIdを省く
+    const { principalId, ...resultWithoutPrincipalId } = result; 
+
+    expect(resultWithoutPrincipalId).toEqual(expected);
+
+  });
+});
+
